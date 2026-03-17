e20f2b693d03d927459f882b85045c45adb32b89
root <root@cv5985175.novalocal>
2026-03-17 13:48:36 +0300

    updated reviews

--- core/serializers.py
+++ core/serializers.py
@@ -8,6 +8,11 @@ class CustomerReviewSerializer(serializers.ModelSerializer):
         read_only_fields = ['submitted_at', 'published_at', 'status']
 
 class ThankYouLetterSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model = ThankYouLetter
         fields = '__all__'
+
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None
